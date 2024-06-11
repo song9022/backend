79ea410277ec9af71a749db034cb9ce8aab02bbc
song9022 <song9022@naver.com>
2024-06-11 19:26:48 +0900

    240611

---
+++ react/chapter05/hooks/src/App2.js
@@ -0,0 +1,11 @@
+// src/App2.js
+
+import UseCallbackHook from "./components/UseCallbackHook"
+import WordCount from "./components/WordCount"
+
+function App2(){
+    // return <WordCount>You are not going to believe this but...</WordCount>
+    return <UseCallbackHook />
+}
+
+export default App2
