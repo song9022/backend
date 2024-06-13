081e300598cefe9320abd58e0a90aa03a8997a8d
song9022 <song9022@naver.com>
2024-06-13 21:43:19 +0900

    240613

---
+++ spring/spring-demo-1/src/main/java/com/example/SpringDemo1Application.java
@@ -0,0 +1,27 @@
+//src/main/java
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@SpringBootApplication
+public class SpringDemo1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDemo1Application.class, args);
+	}
+	
+	@GetMapping(value="/test")
+	public UserDto test() {
+		UserDto userDto=new UserDto();
+		userDto.setName("김자바");
+		userDto.setAge(20);
+		System.out.println(userDto.toString());
+		return userDto;
+	}
+
+}
+//우클릭 > run as > spring boot app
---
+++ spring/spring-demo-1/src/main/java/com/example/UserDto.java
@@ -0,0 +1,17 @@
+package com.example;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+//Lombok라이브러리
+//이 라이브러리는 반복적인 Getter/Setter, ToString과 같은 
+//반복적인 자바 코드를 컴파일과 개발환경에서도 자동으로 생성해주는 라이브러리입니다.
+
+@Getter
+@Setter
+@ToString
+public class UserDto {
+	private String name;
+	private int age;
+}
